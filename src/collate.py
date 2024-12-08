8587b1c54ef60ca7207967c6500dccc9f5ba7442
Ravi Goparaju <tejagoparaju@gmail.com>
2024-12-08 15:19:28 -0600

    fixing issue for custom collate to handle completely empty batch

--- src/collate.py
+++ src/collate.py
@@ -1,3 +1,4 @@
+import torch
 from torch import Tensor
 from torch.utils.data.dataloader import default_collate
 
@@ -5,6 +6,9 @@ def custom_collate(batch: Tensor) -> Tensor:
     # batch = list(filter(lambda x:x is not None, batch))
     new_batch = []
     for image, label in batch:
-        if image != None:
+        if image is not None:
             new_batch.append((image, label))
+    if not new_batch:
+        return torch.empty((0,))
+
     return default_collate(new_batch)
